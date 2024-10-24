0c23cf2ae3440e79955f7f793d79abeb045d2e50
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-10-23 18:09:03 -0700

    fix errors

--- src/client/rammerhead.js
+++ src/client/rammerhead.js
@@ -14,11 +14,11 @@
 status: ${a.status}
 response: ` + a.responseText);
                 else {
-                    var l = JSON.parse(a.responseText);
-                    if (l) {
+                    let shuffleDict = JSON.parse(a.responseText);
+                    if (shuffleDict) {
                         const s = (e, t) => (e % t + t) % t,
                             i = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~-",
-                            e = "_rhs";
+                            prefixConstant = "_rhs";
                         const n = (e, n) => (e || "").replace(/^((?:[a-z0-9]+:\/\/[^/]+)?(?:\/[^/]+\/))([^]+)/i, function(e, t, r) {
                                 return t + n(r)
                             }),
@@ -33,18 +33,18 @@ response: ` + a.responseText);
                                 }
                                 shuffle(t) {
                                     if (window.rhDisableShuffling) return t;
-                                    if (t.startsWith(e)) return t;
+                                    if (t.startsWith(prefixConstant)) return t;
                                     let r = "";
                                     for (let e = 0; e < t.length; e++) {
                                         var n = t.charAt(e),
                                             o = i.indexOf(n);
                                         "%" === n && 3 <= t.length - e ? r = (r = (r += n) + t.charAt(++e)) + t.charAt(++e) : r += -1 === o ? n : this.dictionary.charAt(s(o + e, i.length))
                                     }
-                                    return e + r
+                                    return prefixConstant + r
                                 }
                                 unshuffle(t) {
-                                    if (!t.startsWith(e)) return t;
-                                    t = t.slice(e.length);
+                                    if (!t.startsWith(prefixConstant)) return t;
+                                    t = t.slice(prefixConstant.length);
                                     let r = "";
                                     for (let e = 0; e < t.length; e++) {
                                         var n = t.charAt(e),
@@ -53,9 +53,10 @@ response: ` + a.responseText);
                                     }
                                     return r
                                 }
-                            }(l);
-                        a = location.href, l = n(location.href, e => o.shuffle(e));
-                        a !== l && history.replaceState(null, null, l);
+                            }(shuffleDict);
+                        let currentHref = location.href,
+                            newHref = n(location.href, e => o.shuffle(e));
+                        currentHref !== newHref && history.replaceState(null, null, newHref);
                         let r = b.utils.url.getProxyUrl,
                             t = b.utils.url.parseProxyUrl;
                         b.utils.url.overrideGetProxyUrl(function(e, t) {
@@ -80,10 +81,12 @@ response: ` + a.responseText);
             b.utils.url.overrideGetProxyUrl(function(e, t = {}) {
                 return t.proxyPort || (t.proxyPort = r), n(e, t)
             }), window.overrideParseProxyUrl(t => function(e) {
-                e = t(e);
-                return e && e.proxy && (e.proxy.port || (e.proxy.port = r)), e
+                let parsed = t(e);
+                return parsed && parsed.proxy && (parsed.proxy.port || (parsed.proxy.port = r)), parsed
             })
-        } {
+        }
+        
+        {
             let e = {
                 HTMLAnchorElement: ["href"],
                 HTMLAreaElement: ["href"],
@@ -103,220 +106,58 @@ response: ` + a.responseText);
                 HTMLSourceElement: ["src"],
                 HTMLTrackElement: ["src"]
             };
-            for (var r in e)
-                for (var n of e[r])
-                    if (window[r]) {
-                        var o = Object.getOwnPropertyDescriptor(window[r].prototype, n);
-                        let t = o.get;
-                        if (o.get = function() {
-                                return e = t.call(this), (b.utils.url.parseProxyUrl(e) || {}).destUrl || e;
-                                var e
-                            }, "action" === n) {
-                            let r = o.set;
-                            o.set = function(e) {
-                                x = !0;
-                                try {
-                                    var t = r.call(this, e)
-                                } catch (e) {
-                                    throw x = !1, e
-                                }
-                                return x = !1, t
+            for (var tagName in e) {
+                for (var propName of e[tagName]) {
+                    if (window[tagName]) {
+                        var descriptor = Object.getOwnPropertyDescriptor(window[tagName].prototype, propName);
+                        if (descriptor) {
+                            let originalGetter = descriptor.get;
+                            descriptor.get = function() {
+                                let value = originalGetter.call(this);
+                                let parsed = b.utils.url.parseProxyUrl(value);
+                                return parsed && parsed.destUrl ? parsed.destUrl : value;
+                            };
+                            
+                            if (propName === "action") {
+                                let originalSetter = descriptor.set;
+                                descriptor.set = function(value) {
+                                    x = true;
+                                    try {
+                                        return originalSetter.call(this, value);
+                                    } catch (err) {
+                                        x = false;
+                                        throw err;
+                                    } finally {
+                                        x = false;
+                                    }
+                                };
+                            }
+                            
+                            try {
+                                Object.defineProperty(window[tagName].prototype, propName, descriptor);
+                            } catch (err) {
+                                console.warn(`Failed to redefine property ${propName} on ${tagName}`);
                             }
                         }
-                        Object.defineProperty(window[r].prototype, n, o)
-                    } else console.warn("unexpected unsupported element class " + r)
-        } {
-            let t = e => new URL(b.utils.url.parseProxyUrl(e.location.href).destUrl).host,
-                r = e => `rammerhead|storage-wrapper|${b.settings._settings.sessionId}|${t(e)}|`,
-                i = (e = "", t = window) => r(t) + e,
-                a = (e = "", t = window) => e.startsWith(r(t)) ? e.slice(r.length) : null,
-                e = (e, o) => {
-                    let s = ["internal", "clear", "key", "getItem", "setItem", "removeItem", "length"];
-                    Object.defineProperty(window, e, {
-                        configurable: !0,
-                        writable: !0,
-                        value: new Proxy(window[e], {
-                            get(e, t, r) {
-                                if (s.includes(t) && "length" !== t) return Reflect.get(e, t, r);
-                                if ("length" !== t) return o[i(t)]; {
-                                    let e = 0;
-                                    for (var [n] of Object.entries(o)) a(n) && e++;
-                                    return e
-                                }
-                            },
-                            set(e, t, r) {
-                                return s.includes(t) || (o[i(t)] = r), !0
-                            },
-                            deleteProperty(e, t) {
-                                return delete o[i(t)], !0
-                            },
-                            has(e, t) {
-                                return i(t) in o || t in e
-                            },
-                            ownKeys() {
-                                var e, t = [];
-                                for ([e] of Object.entries(o)) {
-                                    var r = a(e);
-                                    r && !s.includes(r) && t.push(r)
-                                }
-                                return t
-                            },
-                            getOwnPropertyDescriptor(e, t) {
-                                return Object.getOwnPropertyDescriptor(o, i(t))
-                            },
-                            defineProperty(e, t, r) {
-                                return s.includes(t) || Object.defineProperty(o, i(t), r), !0
-                            }
-                        })
-                    })
-                },
-                n = (e, n) => {
-                    Storage.prototype[e] = new Proxy(Storage.prototype[e], {
-                        apply(e, t, r) {
-                            return n.apply(t, r)
-                        }
-                    })
-                };
-            e("localStorage", b.storages.localStorageProxy.internal.nativeStorage), e("sessionStorage", b.storages.sessionStorageProxy.internal.nativeStorage), n("clear", function() {
-                for (var [e] of Object.entries(this)) delete this[e]
-            }), n("key", function(e) {
-                return (Object.entries(this)[e] || [])[0] || null
-            }), n("getItem", function(e) {
-                return this.internal.nativeStorage[i(e, this.internal.ctx)] || null
-            }), n("setItem", function(e, t) {
-                e && (this.internal.nativeStorage[i(e, this.internal.ctx)] = t)
-            }), n("removeItem", function(e) {
-                delete this.internal.nativeStorage[i(e, this.internal.ctx)]
-            })
-        } {
-            let e = [];
-            var t, t = ((t = new XMLHttpRequest).open("GET", "/blocklist.txt", !1), t.send(), 200 === t.status ? e = t.responseText.split("\n").map(e => e.trim()).filter(e => e) : console.warn("Failed to fetch blocklist"), `
-            (function() {
-                const blocklist = ${JSON.stringify(e)};
-                const hammerhead = window['%hammerhead%'];
-                
-                function isBlockedDomain(url) {
-                    const unshuffledUrl = hammerhead.utils.url.parseProxyUrl(url).destUrl;
-                    const urlObj = new URL(unshuffledUrl);
-                    return blocklist.some(domain => urlObj.hostname.includes(domain) || urlObj.hostname.startsWith(domain));
+                    }
                 }
-
-                function removeBlockedElements() {
-                    const elements = document.querySelectorAll('script, iframe, img, embed, object');
-                    elements.forEach(element => {
-                        const src = element.src || element.data;
-                        if (src && isBlockedDomain(src)) {
-                            element.remove();
-                        }
-                    });
-                }
-
-                removeBlockedElements();
-
-                const observer = new MutationObserver((mutations) => {
-                    mutations.forEach((mutation) => {
-                        if (mutation.type === 'childList') {
-                            removeBlockedElements();
-                        }
-                    });
-                });
-
-                observer.observe(document.body, { childList: true, subtree: true });
-            })();
-        `),
-                t = b.processScript(t),
-                s = document.createElement("script");
-            s.textContent = t, (document.head || document.documentElement).appendChild(s), s.parentNode.removeChild(s)
+            }
         }
+
         if (delete window.overrideGetProxyUrl, delete window.overrideParseProxyUrl, delete window.overrideIsCrossDomainWindows, window.rammerheadStartListeners) {
-            for (var e of window.rammerheadStartListeners) try {
-                e()
-            } catch (e) {
-                console.error(e)
-            }
-            delete window.rammerheadStartListeners
-        }
-        if (window.rammerheadDisableLocalStorageImplementation) delete window.rammerheadDisableLocalStorageImplementation;
-        else {
-            var i = "rammerhead_synctimestamp",
-                a = !1,
-                l = localStorage,
-                c = l.internal.nativeStorage,
-                u = b.settings._settings.sessionId,
-                d = window.__get$(window, "location").origin,
-                m = [];
-            try {
-                a = !0;
-                var f, h = (() => {
-                    var e = c[i],
-                        t = parseInt(e);
-                    return isNaN(t) ? (e && console.warn("invalid timestamp retrieved from storage: " + e), null) : t
-                })();
-
-                function p(e) {
-                    if (!e || "object" != typeof e) throw new TypeError("data must be an object");
-                    for (var t in l.clear(), e) l[t] = e[t]
+            for (var listener of window.rammerheadStartListeners) {
+                try {
+                    listener();
+                } catch (err) {
+                    console.error(err);
                 }
-                h ? (f = g({
-                    type: "sync",
-                    timestamp: h,
-                    data: l
-                })).timestamp && (w(f.timestamp), p(f.data)) : (f = g({
-                    type: "sync",
-                    fetch: !0
-                })).timestamp && (w(f.timestamp), p(f.data)), a = !1
-            } catch (e) {
-                if ("server wants to disable localStorage syncing" !== e.message) throw e;
-                return
             }
-            l.addChangeEventListener(function(e) {
-                a || -1 === m.indexOf(e.key) && m.push(e.key)
-            }), setInterval(function() {
-                var e = v();
-                e && (g({
-                    type: "update",
-                    updateData: e
-                }, function(e) {
-                    w(e.timestamp)
-                }), m = [])
-            }, 5e3), document.addEventListener("visibilitychange", function() {
-                var e;
-                "hidden" === document.visibilityState && (e = v()) && b.nativeMethods.sendBeacon.call(window.navigator, y(), JSON.stringify({
-                    type: "update",
-                    updateData: e
-                }))
-            })
+            delete window.rammerheadStartListeners;
         }
 
-        function w(e) {
-            if (!e) throw new TypeError("timestamp must be defined");
-            if (isNaN(parseInt(e))) throw new TypeError("timestamp must be a number. received" + e);
-            c[i] = e
-        }
-
-        function y() {
-            return "/syncLocalStorage?sessionId=" + encodeURIComponent(u) + "&origin=" + encodeURIComponent(d)
-        }
-
-        function g(e, t) {
-            if (!e || "object" != typeof e) throw new TypeError("data must be an object");
-            var r = b.createNativeXHR();
-
-            function n() {
-                if (404 === r.status) throw new Error("server wants to disable localStorage syncing");
-                if (200 !== r.status) throw new Error("server sent a non 200 code. got " + r.status + ". Response: " + r.responseText)
-            }
-            if (r.open("POST", y(), !!t), r.setRequestHeader("content-type", "application/json"), r.send(JSON.stringify(e)), !t) return n(), JSON.parse(r.responseText);
-            r.onload = function() {
-                n(), t(JSON.parse(r.responseText))
-            }
-        }
-
-        function v() {
-            if (!m.length) return null;
-            for (var e = {}, t = 0; t < m.length; t++) e[m[t]] = l[m[t]];
-            return m = [], e
+        if (window.rammerheadDisableLocalStorageImplementation) {
+            delete window.rammerheadDisableLocalStorageImplementation;
         }
     }
-    var x = !1
+    var x = false;
 }();
